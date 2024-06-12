814f50a50cc593a8e40df7759c441398d1f08bb4
Allan Fudal <allanfudal21@gmail.com>
2024-06-12 09:04:06 -0300

    feat: add route to upadte a task by id

--- index.js
+++ index.js
@@ -45,6 +45,30 @@ app.post("/tasks", async (req, res) => {
     }
 });
 
+app.patch("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const taskData = req.body;
+        const taskToUpdate = await TaskModel.findById(taskId);
+
+        const allowedUpdates = ["isCompleted"];
+        const requestedUpdates = object.keys(taskData);
+
+        for (upadate of requestedUpdates) {
+            if (allowedUpdates.includes(update)) {
+                taskToUpdate[update] = taskData[update];
+            } else {
+                return res.status(500).send("Invalid update");
+            }
+        }
+
+        await taskToUpdate.save();
+        return res.status(200).send(taskToUpdate);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
 app.delete("/tasks/:id", async (req, res) => {
     try {
         const taskId = req.params.id;
