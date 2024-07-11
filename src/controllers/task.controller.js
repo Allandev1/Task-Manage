15e2493dbb6b1c891fd162dba71dbf6a09465256
Allan Fudal <allanfudal21@gmail.com>
2024-07-11 10:59:38 -0300

    refactor: move object id cast error to a separate file

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,5 +1,5 @@
 const TaskModel = require("../models/task.model");
-const { notFoundError } = require("../errors/mongodb.error");
+const { notFoundError, objectIdCastError } = require("../errors/mongodb.error");
 const { notAllowedFieldsToUpdateError } = require("../errors/general.erros");
 class TaskController {
     constructor(req, res) {
@@ -25,6 +25,9 @@ class TaskController {
 
             this.res.status(200).send(task);
         } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                return objectIdCastError(this.res);
+            }
             res.status(500).send(error.message);
         }
     }
@@ -64,6 +67,9 @@ class TaskController {
             await taskToUpdate.save();
             return this.res.status(200).send(taskToUpdate);
         } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                return objectIdCastError(this.res);
+            }
             return this.res.status(500).send(error.message);
         }
     }
@@ -80,6 +86,9 @@ class TaskController {
 
             this.res.status(200).send(deletedTask);
         } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                return objectIdCastError(this.res);
+            }
             this.res.status(500).send(error.message);
         }
     }
